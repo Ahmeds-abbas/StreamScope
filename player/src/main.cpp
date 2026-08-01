b1d4b8c4e0acbb984dce642d2d5952468c64f801
Ahmed Elawad <ahmedelawad.abbas@gmail.com>
2026-08-01 05:37:50 +0300

    feat: add initial GStreamer player entry point

--- player/src/main.cpp
+++ player/src/main.cpp
@@ -1,7 +1,20 @@
+#include <gst/gst.h>
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "StreamScope environment ready.\n";
+    gst_init(&argc, &argv);
+
+    if (argc < 2)
+    {
+        std::cerr << "Usage: streamscope_player <HLS URL>\n";
+        return 1;
+    }
+
+    const std::string streamUrl = argv[1];
+
+    std::cout << "Stream URL: " << streamUrl << '\n';
+
     return 0;
 }
