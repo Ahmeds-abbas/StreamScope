8c433c1677566cfdfa857770dcac8a219519728d
Ahmed Elawad <ahmedelawad.abbas@gmail.com>
2026-08-06 03:28:11 +0300

    feat: complete minimal GStreamer HLS player

--- player/src/main.cpp
+++ player/src/main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char* argv[])
 {
+    int exitCode = 0;
     gst_init(&argc, &argv);
 
     if (argc < 2)
@@ -15,86 +16,87 @@ int main(int argc, char* argv[])
     const std::string streamUrl = argv[1];
 
     GstElement* player =
-    gst_element_factory_make("playbin", "streamscope-player");
+        gst_element_factory_make("playbin", "streamscope-player");
 
-if (player == nullptr)
-{
-    std::cerr << "Failed to create GStreamer playbin.\n";
-    return 1;
-}
-g_object_set(player, "uri", streamUrl.c_str(), nullptr);
+    if (player == nullptr)
+    {
+        std::cerr << "Failed to create GStreamer playbin.\n";
+        return 1;
+    }
+    g_object_set(player, "uri", streamUrl.c_str(), nullptr);
 
     std::cout << "Stream URL: " << streamUrl << '\n';
-const GstStateChangeReturn stateResult =
-    gst_element_set_state(player, GST_STATE_PLAYING);
+    const GstStateChangeReturn stateResult =
+        gst_element_set_state(player, GST_STATE_PLAYING);
 
-if (stateResult == GST_STATE_CHANGE_FAILURE)
-{
-    std::cerr << "Failed to start playback.\n";
-    gst_object_unref(player);
-    return 1;
-}
-GstBus* bus = gst_element_get_bus(player);
-
-GstMessage* message = gst_bus_timed_pop_filtered(
-    bus,
-    GST_CLOCK_TIME_NONE,
-    static_cast<GstMessageType>(
-        GST_MESSAGE_ERROR | GST_MESSAGE_EOS
-    )
-);
-
-if (message != nullptr)
-{
-    switch (GST_MESSAGE_TYPE(message))
+    if (stateResult == GST_STATE_CHANGE_FAILURE)
     {
-        case GST_MESSAGE_EOS:
-            std::cout << "Playback finished successfully.\n";
-            break;
+        std::cerr << "Failed to start playback.\n";
+        gst_object_unref(player);
+        return 1;
+    }
+    GstBus* bus = gst_element_get_bus(player);
 
-        case GST_MESSAGE_ERROR:
+    GstMessage* message = gst_bus_timed_pop_filtered(
+        bus,
+        GST_CLOCK_TIME_NONE,
+        static_cast<GstMessageType>(
+            GST_MESSAGE_ERROR | GST_MESSAGE_EOS
+        )
+    );
+
+    if (message != nullptr)
+    {
+        switch (GST_MESSAGE_TYPE(message))
         {
-            GError* error = nullptr;
-            gchar* debugInfo = nullptr;
+            case GST_MESSAGE_EOS:
+                std::cout << "Playback finished successfully.\n";
+                break;
 
-            gst_message_parse_error(
-                message,
-                &error,
-                &debugInfo
-            );
-
-            std::cerr << "Playback failed: "
-                      << error->message
-                      << '\n';
-
-            if (debugInfo != nullptr)
+            case GST_MESSAGE_ERROR:
             {
-                std::cerr << "Debug information: "
-                          << debugInfo
+                GError* error = nullptr;
+                gchar* debugInfo = nullptr;
+
+                gst_message_parse_error(
+                    message,
+                    &error,
+                    &debugInfo
+                );
+
+                std::cerr << "Playback failed: "
+                          << error->message
                           << '\n';
+
+                if (debugInfo != nullptr)
+                {
+                    std::cerr << "Debug information: "
+                              << debugInfo
+                              << '\n';
+                }
+
+                g_clear_error(&error);
+                g_free(debugInfo);
+                exitCode = 1;
+
+                break;
             }
 
-            g_clear_error(&error);
-            g_free(debugInfo);
-
-            break;
+            default:
+                break;
         }
 
-        default:
-            break;
+        gst_message_unref(message);
     }
 
-    gst_message_unref(message);
-}
+    gst_object_unref(bus);
 
-gst_object_unref(bus);
+    gst_element_set_state(
+        player,
+        GST_STATE_NULL
+    );
 
-gst_element_set_state(
-    player,
-    GST_STATE_NULL
-);
+    gst_object_unref(player);
 
-gst_object_unref(player);
-
-return 0;
+    return exitCode;
 }
