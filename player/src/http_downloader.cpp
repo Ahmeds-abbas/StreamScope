75b065077c7679d6d4f9bf8913da4db43faf9311
Ahmed Elawad <ahmedelawad.abbas@gmail.com>
2026-08-11 18:55:47 +0300

    feat: add libcurl downloader setup

--- player/src/http_downloader.cpp
+++ player/src/http_downloader.cpp
@@ -1,1 +1,28 @@
 #include "streamscope/http_downloader.hpp"
+
+#include <curl/curl.h>
+#include <vector>
+
+static size_t writeCallback(
+    char* ptr,
+    size_t size,
+    size_t nmemb,
+    void* userdata
+)
+{
+    const size_t totalBytes = size * nmemb;
+
+    auto* data =
+        static_cast<std::vector<unsigned char>*>(userdata);
+
+    const auto* begin =
+        reinterpret_cast<unsigned char*>(ptr);
+
+    data->insert(
+        data->end(),
+        begin,
+        begin + totalBytes
+    );
+
+    return totalBytes;
+}
